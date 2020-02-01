6ebd11b53851f19d378c588b595cf89aee378460
dosoung <soungdo0919@gmail.com>
2020-02-01 18:47:18 +0900

    Login & Logout

--- NowProject/index.js
+++ NowProject/index.js
@@ -5,6 +5,7 @@ var methodOverride = require('method-override');
 var app = express();
 var flash = require('connect-flash');
 var session = require('express-session')
+var passport = require('./config/passport');
 
 //DB setting
 mongoose.set('useNewUrlParser', true);
@@ -32,17 +33,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(flash());
-app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg')
-    res.locals.error = req.flash('error')
 
-    next();
-});
+//Passport
+app.use(passport.initialize());  //Passport를 초기화
+app.use(passport.session());    //passport와 session을 연결시켜줌
+
+app.use(function(req,res,next) {
+  res.locals.isAuthenticated = req.isAuthenticated(); //passport제공 함수 로그인이 되어있는지 아닌지 true , false
+  res.locals.currentUser = req.user; //req.user는 passport에서 추가하는 항목으로 로그인이 되면 session으로 부터 user를 deserialize하여 생성
+  next();   //req.locals담겨진 변수는 ejs사용 가능하다.
+})
 //Routes
 app.use('/', require('./routes/home'));
 app.use('/posts' , require('./routes/posts'));
 app.use('/users', require('./routes/users'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, console.log(`Server connected on port ${PORT}`));
